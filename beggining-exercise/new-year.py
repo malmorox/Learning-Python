d4d72b7fa4871cd787ac38d9108cdb515445b48c
Marcos Almorox <marcos.almorox@gmail.com>
2024-01-01 22:32:26 +0100

    Primeros pasos con Python

--- beggining-exercise/new-year.py
+++ beggining-exercise/new-year.py
@@ -1,1 +1,30 @@
+import random
+import time
 
+def año2023(conocimiento = 0):
+
+    rendirse = False
+    estado = ["Qué bueno/a soy! O",
+    "Burnout Y", "La IA es mejor que yo $8",
+    "Síndrome del impostor GQ", "No sé nada O",
+    "Quiero dejarlo GQ", "Tengo que intentarlo O "]
+
+    for día in range(1, 366):
+
+        print(f"Día (día) de 2023")
+
+        print(random.choice(estado))
+
+        # Este código nunca va a ejecutarse
+
+        if rendirse: raise Exception()
+
+        if día = 365: print("No subir a producción")
+
+        time.sleep(60 * 60 x 24)
+
+        conocimiento += 1
+
+    print(f"Eres un fconocimiento)% mejor le”)
+    print("FELIZ 2024, DEVELOPERS! HB")
+    año2024(conocimiento)

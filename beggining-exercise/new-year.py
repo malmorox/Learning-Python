78926d34a3a2ab6915d8c82e25908bd15de463fc
Marcos Almorox <marcos.almorox@gmail.com>
2024-01-02 12:36:38 +0100

    Cambios y configurando interprete

--- beggining-exercise/new-year.py
+++ beggining-exercise/new-year.py
@@ -5,11 +5,11 @@ def año2024(conocimiento = 0):
 
     rendirse = False
     estado = ["Qué bueno/a soy! O",
-    "Burnout Y", "La IA es mejor que yo $8",
-    "Síndrome del impostor GQ", "No sé nada O",
-    "Quiero dejarlo GQ", "Tengo que intentarlo O "]
+            "Burnout Y", "La IA es mejor que yo $8",
+            "Síndrome del impostor GQ", "No sé nada O",
+            "Quiero dejarlo GQ", "Tengo que intentarlo O "]
 
-    for día in range(1, 366):
+    for dia in range(1, 366):
 
         print(f"Día (día) de 2024")
 
@@ -19,12 +19,12 @@ def año2024(conocimiento = 0):
 
         if rendirse: raise Exception()
 
-        if día == 365: print("No subir a producción")
+        if dia == 365: print("No subir a producción")
 
         time.sleep(60 * 60 * 24)
 
         conocimiento += 1
 
-    print(f"Eres un fconocimiento)% mejor")
+    print(f"Eres un {conocimiento}% mejor")
     print("FELIZ 2024, DEVELOPERS! HB")
     año2024(conocimiento)
